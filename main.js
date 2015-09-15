b27156f584df98e96ad4345f6a183455143eca9c
=Andy Gross <agross@colorado.edu>
2015-09-15 13:36:49 -0600

    Added windows instructions for console

--- main.js
+++ main.js
@@ -5,15 +5,45 @@
 
 //1. Log your name to the console (as a string).
 
+var string ="Andy";
+
 //2. Declare a variable called name, log it to the console.
+
+var name = "Andy"
+console.log(name);
 
 //3. Declare two variables with number values. Add, subtract, multiply and divide them.
 
+var number1 = 5;
+var number2 = 2;
+var add  = (number1 + number2);
+var sub = (number1 - number2);
+var mult = (number1 * number2);
+var div = (number1 / number2);
+
 //4. Declare two variables with string values. Create a third variable that concatenates them.
+
+var tasty ='moldy';
+var type ='cheese';
+var smell= tasty + type;
+
 
 //5. Add, subract, multiply and divide the strings '5' and '3'. Observe and explain the results.
 
+console.log('5' + '3');
+console.log('5' - '3');
+console.log('5' * '3');
+console.log('5' / '3');
+
+1) 53
+2) 2
+3) 15
+4) 1.6
+
 //6. Create a prompt that asks a user to pick a number. Double the answer.
+
+
+
 
 //7. Create a custom alert.
 
