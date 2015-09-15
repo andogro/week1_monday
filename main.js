2b2b6641d3f06cd2ff177f5bc5a9e35ebdc0ed71
=Andy Gross <agross@colorado.edu>
2015-09-15 14:19:14 -0600

    Main answers

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 
 //1. Log your name to the console (as a string).
 
-var string ="Andy";
+console.log('Andy');
 
 //2. Declare a variable called name, log it to the console.
 
@@ -35,21 +35,37 @@ console.log('5' - '3');
 console.log('5' * '3');
 console.log('5' / '3');
 
-1) 53
-2) 2
-3) 15
-4) 1.6
+1) 53 creates a 2 digit number from the single numbers
+2) 2 works fine
+3) 15 works fine
+4) 1.6 works fine
 
 //6. Create a prompt that asks a user to pick a number. Double the answer.
 
-
-
+var numberPick = prompt('Enter a number any number');
+var dub = numberPick * 2;
 
 //7. Create a custom alert.
 
+alert('Don\'t Look Up, That\'s my dog!')
+
 //8. Declare three variables called length, width and height. Calculate area and volume.
+
+var length = 5;
+var width = 10;
+var height = 15;
+var area = (length * width);
+var volume = (height * width * length) + ' Cubic Inches';
 
 //9. Ask for a user's name, age and city. In a single command display/log the sentence:
 // "My name is...I'm....years old, and I live in....."
 
+var name1 = prompt('Enter your Name')
+var age1 = prompt('How old are you?')
+var city1 = prompt('What city do you live in')
+
+console.log('Your Name is ' + name1 + '.  You Are ' + age1 + ' years old, and you live in '+ city1 '.');
+
 //10. Create an alert that uses the response from a prompt.
+var Dontdoit = prompt('Enter something here');
+alert(Dontdoit);
